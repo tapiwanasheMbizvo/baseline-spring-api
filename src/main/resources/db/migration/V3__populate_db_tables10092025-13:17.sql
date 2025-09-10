75efcf8d7680bcf6d7d5e7fcb440e61aacfa2e81
tapiwa <tapiwambizvo@gmial.com>
2025-09-10 18:21:37 +0200

    build and push to ecr only

---
+++ src/main/resources/db/migration/V3__populate_db_tables10092025-13:17.sql
@@ -0,0 +1,3 @@
+
+insert into  characters (firstName, lastName, email) values ('taku', 'rango', 'taku-rango@gmail.com');
+insert into  characters (firstName, lastName, email) values ('bob', 'marly', 'bob-marly@gmail.com')
