6cf2cf091f423e82ee398967229b5c41bc832ef7
tapiwa <tapiwambizvo@gmial.com>
2025-09-16 21:23:23 +0200

    json converter

--- src/main/java/com/tapiwanashe/mbizvo/baseline/api/config/RabbitConfig.java
+++ src/main/java/com/tapiwanashe/mbizvo/baseline/api/config/RabbitConfig.java
@@ -1,6 +1,7 @@
 package com.tapiwanashe.mbizvo.baseline.api.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +14,10 @@ public class RabbitConfig {
     @Bean
     public Queue demoQueue() {
         return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
