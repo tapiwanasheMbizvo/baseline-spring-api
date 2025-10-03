57671f9d48b1eb58285b7eb0e74f990a5eaf9393
tapiwa <tapiwambizvo@gmial.com>
2025-10-03 18:46:35 +0200

    update and deploy.

--- src/main/java/com/tapiwanashe/mbizvo/baseline/api/controllers/HelloWorld.java
+++ src/main/java/com/tapiwanashe/mbizvo/baseline/api/controllers/HelloWorld.java
@@ -20,7 +20,7 @@ public class HelloWorld {
         return  ApiResponse.<String>builder()
                 .data("hello world from api")
                 .status(HttpStatus.ACCEPTED)
-                .message("hello world from api running  in ecs")
+                .message("hello world from api running , let update this and redeploy....")
                 .timestamp(LocalDateTime.now())
                 .build();
     }
