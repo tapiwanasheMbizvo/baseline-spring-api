e04e3de7472447e4292793f8bb6f4737b249ac9d
tapiwa <tapiwambizvo@gmial.com>
2025-10-03 21:19:32 +0200

    add to gitignore

--- src/main/java/com/tapiwanashe/mbizvo/baseline/api/repositories/IconCharacterRepository.java
+++ src/main/java/com/tapiwanashe/mbizvo/baseline/api/repositories/IconCharacterRepository.java
@@ -2,7 +2,6 @@ package com.tapiwanashe.mbizvo.baseline.api.repositories;
 
 import com.tapiwanashe.mbizvo.baseline.api.entity.IconicCharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface IconCharacterRepository extends JpaRepository<IconicCharacter, Long> {
 }
